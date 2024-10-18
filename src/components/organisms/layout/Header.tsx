8e59ceb5dcb237fd09772c8650d95e48d5b31706
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-18 21:41:07 +0900

    ヘッターの画面遷移

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,10 +1,16 @@
 import { Box,Flex,Heading,Link, useDisclosure } from "@chakra-ui/react";
-import { memo,VFC } from "react"
+import { memo,useCallback,VFC } from "react"
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { useNavigate } from "react-router-dom";
 
 export const Header: VFC = memo(() => {
     const {isOpen,onOpen,onClose} = useDisclosure();
+    const navigate = useNavigate()
+    const onClickHome = useCallback(() => navigate("/home"),[navigate]);
+    const onClickUserManagement = useCallback(() => navigate("/user_management"),[navigate]);
+    const onClickSetting = useCallback(() => navigate("/setting"),[navigate]);
+
     return (
         <>
         <Flex
@@ -21,7 +27,7 @@ export const Header: VFC = memo(() => {
             zIndex={1} // ヘッダーが常に最前面に表示されるように
         >
 
-        <Heading as = "h1" fontSize={{ base: "md", md: "lg" }}> 
+        <Heading as = "h1" fontSize={{ base: "md", md: "lg" }} onClick={onClickHome}> 
             ユーザー管理アプリ
         </Heading>
         <Flex 
@@ -31,9 +37,9 @@ export const Header: VFC = memo(() => {
             display={{ base:"none",md:"flex" }}
             >
             <Box pr={4}>
-                <Link>ユーザー一覧</Link>      
+                <Link onClick={onClickUserManagement }>ユーザー一覧</Link>      
             </Box>
-            <Link>設定</Link>
+            <Link onClick = {onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen}/>
     </Flex>
