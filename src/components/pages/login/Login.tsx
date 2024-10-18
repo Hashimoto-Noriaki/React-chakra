32536ece1422966b670e496e2386a9b73429d26e
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-19 00:51:41 +0900

    ログインの雛形

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,5 +1,15 @@
 import { memo,VFC } from "react"
+import { Box,Button,Divider,Flex,Heading,Input } from "@chakra-ui/react";
 
 export const Login: VFC = memo(() => {
-    return <p>ログインページ</p>
+    return (
+        <Flex>
+            <Box>
+                <Heading as="h1">ユーザー管理アプリ</Heading>
+                <Divider />
+                <Input placeholder="ユーザーID"/>
+                <Button>ログイン</Button>
+            </Box>
+        </Flex>
+    )
 });
