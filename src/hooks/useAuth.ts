82bc964ed4e6fef3d19d869e9225f8961d6e3550
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-20 23:31:07 +0900

    apiを呼ぶ実装

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,20 @@
+import { useCallback } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import { User } from "../types/api/user";
+
+export const useAuth = () => {
+    const navigate = useNavigate();
+    const login = useCallback((id:string) => {
+        axios
+            .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then(res => {
+                if(res.data) {
+                    navigate("/home"); 
+                } else {
+                    alert("ユーザーが見つかりません")
+                }
+            }).catch(() => alert("ログインできません"));
+        },[navigate])
+    return { login }
+}
